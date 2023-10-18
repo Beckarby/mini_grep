fd5bf8880cddbc988ad0d6f4babe777379af452c
Rebecca Bracho <rebeccabracho05@gmail.com>
2023-10-18 14:45:13 -0400

    its maybe complete... maybe

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <windows.h>
 #include <algorithm>
-
+#include <sstream>
 
 
 using namespace std;
@@ -17,50 +17,72 @@ int main(int argc, char** argv){
     char color_of_choice = ' ';
     color_of_choice = *argv[3];                        //color del texto encontrado
     string linea;
+    int parrafos = 0;
+    int cont = 0;
+
     ifstream infile(nombre_archivo);                    
     if(!infile){
-        cout << "No se ha encontrado el archivo" << endl;
+        std::cout << "No se ha encontrado el archivo" << endl;
         return 0;
     }
+   
+    while(getline(infile, linea)){                //busca la palabra u expresion sin importan mayus o minus
+        size_t pos = linea.find(busqueda);
 
-    while(getline(infile, linea)){                      //busca la palabra u expresion sin importan mayus o minus
-        if (search(linea.begin(), linea.end(), busqueda.begin(), busqueda.end(),          
-            [](char c1, char c2) { return std::tolower(c1) == std::tolower(c2); }) != linea.end()) {
-            switch (color_of_choice)
-            {
-            case '1':
-                console_color(1);   //blue
-                break;
-            case '2':
-                console_color(2); //green
-                break;
-            case '3':
-                console_color(3); //light blue or cya
-                break;
-            case '4':
-                console_color(4); //red
-                break;
-            case '5':
-                console_color(5); //purple
-                break;
-            case '6':
-                console_color(6); //yellow
-                break;
-            case '7':
-                console_color(7); //white
-                break;
-            case '8':
-                console_color(8); //grey
-                break;
-            default:
-                console_color(7); //default white
-                break;
+            if (search(linea.begin(), linea.end(), busqueda.begin(), busqueda.end(),          
+                [](char c1, char c2) { return std::tolower(c1) == std::tolower(c2); }) != linea.end()) {
+                    
+                    cout << linea.substr(0, pos);
+                                    
+                    //coincidencias = std::count(busqueda.begin(), busqueda.end(), linea);
+                if (pos != string::npos) {
+                    switch (color_of_choice) {
+                        case '1':
+                            console_color(1);   // blue
+                            break;
+                        case '2':
+                            console_color(2); // green
+                            break;
+                        case '3':
+                            console_color(3); // light blue or cyan
+                            break;
+                        case '4':
+                            console_color(4); // red
+                            break;
+                        case '5':
+                            console_color(5); // purple
+                            break;
+                        case '6':
+                            console_color(6); // yellow
+                            break;
+                        case '7':
+                            console_color(7); // white
+                            break;
+                        case '8':
+                            console_color(8); // grey
+                            break;
+                        default:
+                            console_color(7); // default white
+                            break;
+                    }
+                    cout << linea.substr(pos, busqueda.length());
+
+                    console_color(7);
+                    cout << linea.substr(pos + busqueda.length()) << endl;
+                    cont++;
+                    //parrafos++;
+                    //std::cout << linea << endl;
+                }           
+                
             }
-             
-            cout << linea << endl;
-        }
+    }
+    if(cont > 1){
+        std::cout << endl <<"Han habido " << cont << " coincidencias";
+    }else {
+        cout << "No se encontraron coincidencias" << endl;
     }
     
-   console_color(7);
+
+    infile.close();
 
 }
